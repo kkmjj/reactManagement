e19951de1e3ac47d4c3d0293becf502704809174
Kim <mjkim1201@naver.com>
2020-01-01 21:11:51 +0900

    node js 로 restAPI 적용하여 화면에 띄우기

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello', (req,res)=>{
-    res.send({message: 'Hello Express!'});
+// client가 customers api에 들어오면
+app.get('/api/customers', (req,res)=>{
+    res.send([{
+        'name': '홍길동',
+        'birthday': '931201'
+      
+      },
+      {
+        'name': '김민준',
+        'birthday': '941201'
+      
+      }]);
 
 });
 

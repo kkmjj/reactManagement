9022d62cfd15820e9469c4323b892c9fe77d7a20
Kim <mjkim1201@naver.com>
2020-01-06 22:22:11 +0900

    aws rds heidisql  node.js 서버에 적용해보기

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs =require('fs');
 const express = require('express');
 const bodyParser =require('body-parser');
 const app = express();
@@ -6,18 +7,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+// 파일 받아서 json으로파싱하고 mysql에서 받아오기 
+const data =fs.readFileSync('./database.json');
+const conf =JSON.parse(data);
+const mysql =require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 // client가 customers api에 들어오면
 app.get('/api/customers', (req,res)=>{
-    res.send([{
-        'name': '홍길동',
-        'birthday': '931201'
+   connection.query(
+     "select * from customer",
+      (err,rows,fields) =>{
+        res.send(rows);
+      }
       
-      },
-      {
-        'name': '김민준',
-        'birthday': '941201'
-      
-      }]);
+   );
 
 });
 
